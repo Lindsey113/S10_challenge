139ecb500fe017c23fc4008b9b21fcc56947e14f
Lindsey <lindsey_mccluskey@yahoo.ca>
2024-08-05 00:34:03 -0700

    wip

--- frontend/state/store.js
+++ frontend/state/store.js
@@ -2,18 +2,19 @@ import { configureStore } from '@reduxjs/toolkit'
 import orderReducer from './orderSlice'
 import { orderApi } from './orderApi'
 
-const exampleReducer = (state = { count: 0 }) => {
-  return state
-}
+// const exampleReducer = (state = { count: 0 }) => {
+//   return state
+// }
 
 export const resetStore = () => configureStore({
   reducer: {
     orderState: orderReducer,
    [orderApi.reducerPath]: orderApi.reducer
   },
-  middleware: getDefault => getDefault().concat(
+  
     // if using RTK Query for your networking: add your middleware here
     // if using Redux Thunk for your networking: you can ignore this
+    middleware: getDefault => getDefault().concat(
     orderApi.middleware
   ),
 })
